de58344542104c3b04055260b796daf102ad21b6
vansmartindo-blip <vansmartindo@gmail.com>
2026-04-27 22:17:09 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   serverExternalPackages: ["jsonwebtoken", "bcryptjs"],
   typescript: {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
-  allowedDevOrigins: ["*"],
 };
 
 export default nextConfig;
