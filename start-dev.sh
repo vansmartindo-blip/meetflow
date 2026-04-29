53bd8877f49d6491684b6b993f50cca5d3ed307c
vansmartindo-blip <vansmartindo@gmail.com>
2026-04-29 10:29:02 +0700

    Delete start-dev.sh

--- start-dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-while true; do
-  echo "[$(date)] Starting Next.js dev server..."
-  bun run dev
-  echo "[$(date)] Next.js dev server exited. Restarting in 2s..."
-  sleep 2
-done
